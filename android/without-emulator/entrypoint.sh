6c70d72f5e7d342ff8182bba0709bcf123b13897
ghostmatem <bebe558812@gmail.com>
2022-03-17 20:28:37 +0300

    add android image

--- android/without-emulator/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-git clone $GIT_LINK ./project
-cd ./project
-flutter clean
-flutter pub get
-flutter test --machine | ~/.pub-cache/bin/tojunit > test-report.xml
-cat test-report.xml
-alex@alex-server:~/projects/flutter-docker/docker-for-tests$
